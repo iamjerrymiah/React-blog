e5710bd8bf7d8743765fbcf70b23549e25c10f1d
Nwafor Jeremiah <email>
2022-03-24 10:23:49 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+//Always return your request
+axios.interceptors.request.use(req =>{
+    console.log(req);
+    return req;
+}, error =>{
+    console.log(error)
+    return Promise.reject(error)
+});
+
+axios.interceptors.response.use(res =>{
+    console.log(res);
+    return res;
+}, error =>{
+    console.log(error)
+    return Promise.reject(error)
+});
+
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerServiceWorker();
